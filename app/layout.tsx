557176e16aa2b4e20da5328d727d3d2d6cc44bcf
ogabrielsv <ogabrielsv.ia@gmail.com>
2026-01-13 20:16:33 -0300

    fav

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "PlayPrêmios",
   description: "Play Prêmios: Participe de sorteios gratuitos!",
+  icons: {
+    icon: 'https://i.imgur.com/UhiADKk.png',
+    shortcut: 'https://i.imgur.com/UhiADKk.png',
+    apple: 'https://i.imgur.com/UhiADKk.png',
+  },
 };
 
 export default function RootLayout({
